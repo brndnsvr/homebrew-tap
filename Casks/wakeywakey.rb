cf405c7e54e02a1d46cf73f7249de84f7c43593f
Brandon Seaver <seaverb@icloud.com>
2025-12-31 12:24:56 -0500

    Update wakeywakey to v1.0.1
    - Now notarized for Gatekeeper compliance
    - No more security warnings on first launch

--- Casks/wakeywakey.rb
+++ Casks/wakeywakey.rb
@@ -1,8 +1,8 @@
 cask "wakeywakey" do
-  version "1.0.0"
-  sha256 "4c75b161be1c2fc83bfa638b4a3f8f23d78aeac52133f24d6684a49df3624e1c"
+  version "1.0.1"
+  sha256 "5671d926f8adf2298ca5c00b5c2552fb87b934f880be94e43fcdca784ec1249c"
 
-  url "https://github.com/brndnsvr/WakeyWakey/releases/download/v#{version}/WakeyWakey-1.0.dmg"
+  url "https://github.com/brndnsvr/WakeyWakey/releases/download/v#{version}/WakeyWakey-#{version}.dmg"
   name "WakeyWakey"
   desc "Menu bar app that keeps your Mac awake with subtle mouse movements"
   homepage "https://github.com/brndnsvr/WakeyWakey"
